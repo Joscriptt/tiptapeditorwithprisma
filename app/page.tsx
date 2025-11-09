7f2f0884724eaec680fb33089072e881aa96f0c4
Joscript <39909905+Joscriptt@users.noreply.github.com>
2025-11-09 14:50:46 +0100

    added simple

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,10 @@ import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor
 import Image from "next/image";
 
 export default function Home() {
-  return <SimpleEditor />;
+  return (
+    <div>
+      <SimpleEditor />
+      hello world
+    </div>
+  );
 }
